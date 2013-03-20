98d0c0ea686ecd67995864ac13f621f719156887
Myles Tan <mylestan7@gmail.com>
2013-03-20 13:15:34 +0800

    added icons to about page

--- assets/about.js
+++ assets/about.js
@@ -3,7 +3,7 @@
 //slides the three steps up and down to reveal more text.
 $(document).ready(function()
 	{
-	$("#details p.accordian-head").click(function()
+	$("#details div.accordian-head").click(function()
 	{
 		$(this).next("div.accordian-body").slideToggle(300);
 	});
